df3166f9b354bb8385331bc4e3d552043ed83a62
SAHIL sanghar <sangharsahil546@gmail.com>
2024-08-20 20:20:06 +0530

    ...

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,23 +1,40 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const Footer = () => {
-return (
-    <div className="flex flex-row justify-between gap-4 py-5 px-10">
-    <div className="flex flex-col items-center">
-        <Image src='/f1.png' alt='icon' height={40} width={40} />
-        <p className="mt-2">Help</p>
-    </div>
-    <div className="flex flex-col items-center">
-        <Image src='/f2.png' alt='icon' height={40} width={40} />
-        <p className="mt-2">App</p>
-    </div>
-    <div className="flex flex-col items-center">
-        <Image src='/f3.png' alt='icon' height={40} width={40} />
-        <p className="mt-2">Services</p>
-    </div>
-    </div>
-)
+    const router = useRouter()
+
+    const handleNavigation = (path) => {
+        router.push(path)
+    }
+
+    return (
+        <div className="flex flex-row justify-between gap-2 py-5">
+            <div
+                className="flex flex-col items-center cursor-pointer"
+                onClick={() => handleNavigation('/')}
+            >
+                <Image src='/f1.png' alt='icon' height={40} width={40} />
+                <p className="mt-2">Help</p>
+            </div>
+            <div
+                className="flex flex-col items-center cursor-pointer"
+                onClick={() => handleNavigation('/product')}
+            >
+                <Image src='/f2.png' alt='icon' height={40} width={40} />
+                <p className="mt-2">App</p>
+            </div>
+            <div
+                className="flex flex-col items-center cursor-pointer"
+                onClick={() => handleNavigation('/login')}
+            >
+                <Image src='/f3.png' alt='icon' height={40} width={40} />
+                <p className="mt-2">Services</p>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
